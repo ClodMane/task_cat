f812f7ea40cc1c4300d39556fc243a6a2b26603f
Artyom <avneretin23@gmail.com>
2022-06-28 23:54:14 +0300

    added fonts configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+      },
+      {
         test: /\.(scss|css)$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
